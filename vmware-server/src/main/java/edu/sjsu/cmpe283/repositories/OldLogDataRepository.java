1b7a10c7e842fa68aac1fe728283c11d9e5c6a44
Sai <srkarra@users.noreply.github.com>
2014-05-05 11:22:56 -0700

    Update OldLogDataRepository.java

--- vmware-server/src/main/java/edu/sjsu/cmpe283/repositories/OldLogDataRepository.java
+++ vmware-server/src/main/java/edu/sjsu/cmpe283/repositories/OldLogDataRepository.java
@@ -1,8 +1,12 @@
 package edu.sjsu.cmpe283.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import edu.sjsu.cmpe283.entities.OldLogData;
 
 public interface OldLogDataRepository extends MongoRepository<OldLogData, Long> {
+
+	List<OldLogData> findByVmNameAndFileName(String vmName, String fileName);
 }
